253ca687053ae7a6630b92eada5be4b59df54935
Just_PlayBoy <urovjohn@gmail.com>
2020-04-29 04:11:36 +0700

    Make column logic
    if someone read it, you must know, that it was the most difficult part of these work, for these implimintation I use unity dock, becouse I just didn't know how make it, but when I read unity documentation my work in it started, I make some videos about my work, it was very fun.

---
+++ TestTask/Assets/Scripts/ColumnSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+    private GameObject oldPrefab;
+    void Start()
+    {
+        StartCoroutine(SpawnColumn());
+    }
+
+    private IEnumerator SpawnColumn()
+    {
+
+        for (; ; )
+        {
+            yield return new WaitForSeconds(2);
+            Vector3 position = new Vector3(Random.Range(10.0F, 15.0F), Random.Range(-1.7F, 1.7F), 0);
+            oldPrefab = Instantiate(prefab, position, Quaternion.identity);
+            Destroy(oldPrefab, 15);
+        }
+    }
+}
