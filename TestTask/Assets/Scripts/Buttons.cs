5c9a836beccb61e64b3fb791c315f8b2303d12e2
Just_PlayBoy <urovjohn@gmail.com>
2020-04-29 17:53:32 +0700

    Finish End scene

---
+++ TestTask/Assets/Scripts/Buttons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Menu()
+    {
+        Debug.Log("yes");
+        //SceneManager.LoadScene(0);
+    }
+    private void Update()
+    {
+        //if (Input.GetMouseButtonDown(0) && tag.Equals("Restart"))
+        //{
+        //    Restart();
+        //}
+        //if (Input.GetMouseButtonDown(0) && tag.Equals("Menu"))
+        //{
+        //    Restart();
+        //}
+    }
+}
