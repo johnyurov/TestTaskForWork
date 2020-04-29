52b70810879b9f0a300d629838323114cdcdf5db
Just_PlayBoy <urovjohn@gmail.com>
2020-04-29 20:21:54 +0700

    Floor ans sky gameover

--- TestTask/Assets/Scripts/SkyAndFloor.cs
+++ TestTask/Assets/Scripts/SkyAndFloor.cs
@@ -1,28 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class ColumnMove : MonoBehaviour
+public class SkyAndFloor : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 2.0F;
-
-    void Update()
-    {
-        transform.Translate(-speed,0,0);
-        
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            speed = 0.0F;
             if (gameObject.tag == "Lose")
             {
-                speed = 0.0F;
+                SceneManager.LoadScene(2);
             }
         }
     }
-    
 }
